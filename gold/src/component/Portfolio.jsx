0e3a9616cfe848bf42367d805118cbfdd09c50db
Saurav <saurav.teknowxa@gmail.com>
2025-09-15 18:12:53 +0530

    update

--- gold/src/component/Portfolio.jsx
+++ gold/src/component/Portfolio.jsx
@@ -3,7 +3,20 @@
 function Portfolio(){
     return(
         <>
-        
+            <div className="w-full mt-36 mb-28">
+                    <div className="bg-white flex flex-col items-center justify-center gap-12 w-full   px-96 ">
+                        <span className="text-xl md:text-6xl text-center font-semibold">Testimonial</span>
+                        <p className="text-xs lg:text-3xl text-gray-400 text-center px-20">Working with this team was a fantastic experience. Their attention to detail and commitment to quality exceeded our expectations.</p>
+                        <p className="text-xs lg:text-3xl text-gray-600 font-semibold text-center px-2">" From the initial consultation to the final delivery, every step was handled professionally. The end result was a product that not only met our needs but also impressed our stakeholders. Highly recommended! "</p>
+                        <p className="text-xs lg:text-3xl text-center">
+                            <span className="text-xl md:text-3xl text-center font-semibold">Saurav Kumar</span>
+                            <h3 className="text-xl md:text-2xl text-center text-gray-400 font-semibold p-3">Full Stack Developer , Teknowxa Company</h3>
+                
+                        </p>
+
+                    </div>
+                </div>
+
         </>
     )
 }
