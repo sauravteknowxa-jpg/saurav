77ef6073d16a2ac8a1db5af8b65a241d42ee5098
Saurav <saurav.teknowxa@gmail.com>
2025-09-15 17:27:00 +0530

    portfolio

--- gold/src/component/Slider_Ex.jsx
+++ gold/src/component/Slider_Ex.jsx
@@ -6,7 +6,7 @@ import { FaBeer } from 'react-icons/fa';
 function Slider(){
   return(
     <>
-      <div>
+      <div className="w-full h-screen flex items-center justify-center ">
          <h1 className="text-4xl text-red-300">Slider <FaBeer /> </h1>
       </div>
     </>
